4209b5d0de10714dea08db5d5e83a2442d31a819
xsling <me@xsl.sh>
2023-11-15 22:36:49 -0800

    feat: refresh imap session

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,54 @@
-use std::{thread, time};
 use chrono::Local;
-
+use tokio::time::{self, Duration, Instant};
 mod config;
 mod email;
 mod register;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let mut interval = time::interval(Duration::from_secs(30));
+    let mut session_reinit_timer = time::interval(Duration::from_secs(2 * 60 * 60));
+
     println!("Logging into IMAP...");
-    match email::init_imap_session() {
-        Ok(mut session) => {
-            loop {
+    let mut session_init_time = Instant::now();
+    let mut session = match email::init_imap_session() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Failed to initialize IMAP session: {}", e);
+            return;
+        }
+    };
+
+    loop {
+        tokio::select! {
+            _ = interval.tick() => {
                 println!("Fetching Email at: {}", Local::now().format("%Y-%m-%d %H:%M:%S"));
                 match email::fetch_email(&mut session) {
                     Ok(r) => println!("{}", r),
-                    Err(err) => eprintln!("{}", err),
+                    Err(err) => eprintln!("Error fetching email: {}", err),
                 }
-                thread::sleep(time::Duration::from_secs(30));
             }
-            // match email::close_imap_session(&mut session) {
-            //     Ok(r) => println!("{}", r),
-            //     Err(err) => eprintln!("{}", err),
-            // }
+            _ = session_reinit_timer.tick() => {
+                if session_init_time.elapsed() < Duration::from_secs(1 * 60 * 60) {
+                    continue;
+                }
+                match email::close_imap_session(&mut session) {
+                    Ok(_) => println!("Session closed."),
+                    Err(err) => eprintln!("Failed to close IMAP session: {}", err),
+                }
+
+                match email::init_imap_session() {
+                    Ok(new_session) => {
+                        println!("IMAP session reinitialized.");
+                        session = new_session;
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to reinitialize IMAP session: {}", e);
+                        return;
+                    }
+                }
+                session_init_time = Instant::now();
+            }
         }
-        Err(e) => eprintln!("Failed to initialize IMAP session: {}", e),
     }
 }
